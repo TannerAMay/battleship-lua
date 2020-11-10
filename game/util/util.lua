79234a53a9164f4a5aff639e3ceff503bb846c20
JayTSmith <justinsmth42@gmail.com>
2020-11-10 16:37:12 -0600

    Documentation

--- game/util/util.lua
+++ game/util/util.lua
@@ -7,7 +7,20 @@ function checkRectCollision(checkX, checkY, x, y, width, height)
     return true
 end
 
+--[[
+    A function to check if a ship is in a valid placement location
+
+    Parameters:
+        cellX: the selected cell x position
+        cellY: the selected cell y position
+        ship: the ship to be placed
+        grid: the shipGrid to place the ship on
+]]
 function canPlaceShip(cellX, cellY, ship, grid)
+    if cellY + ship.length >= #grid then
+        return false
+    if cellX + ship.width >= #grid[1] then
+        return false
     for dx = 0, ship.width - 1 do
         if grid[cellY][cellX + dx] ~= "~" then
             return false
@@ -23,6 +36,14 @@ function canPlaceShip(cellX, cellY, ship, grid)
     return true
 end
 
+--[[
+    A quick and dirty function to quickly construct a square grid
+
+    Parameters:
+        rows: the number of rows in the grid
+        cols: the number of cols in the grid
+        fill: the element to insert into the newly created grid.
+]]
 function makeGrid(rows, cols, fill)
     local grid = {}
     local row = nil
