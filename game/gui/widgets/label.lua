e3f1f5c9cfaffbe752dd3aad341a2f075343b541
JayTSmith <justinsmth42@gmail.com>
2020-11-17 15:51:38 -0600

    More save/load stuff. Label can go invisible now.

--- game/gui/widgets/label.lua
+++ game/gui/widgets/label.lua
@@ -9,6 +9,8 @@ function Label:new(text, x, y, width, color, align)
 
     self.alignment = align or "center"
     self.color = color or love.graphics.getColor()
+
+    self.visible = true
 end
 
 function Label:reset()
@@ -18,6 +20,8 @@ function Label:update(dt)
 end
 
 function Label:draw()
+  if (self.visible) then
     love.graphics.setColor(self.color[1], self.color[2], self.color[3], self.color[4] or 1.0)
     love.graphics.printf(self.text, self.x, self.y, self.width, self.alignment)
+  end
 end
